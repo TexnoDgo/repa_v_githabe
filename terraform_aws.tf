eb73aee20223883f701c97b0f5b66cab6cabbabd
TexnoDgo <texnodgo@gmail.com>
2022-01-24 23:09:49 +0200

    Update terraform_aws.tf

--- terraform_aws.tf
+++ terraform_aws.tf
@@ -73,3 +73,15 @@ EOF
 #  allocation_id = eipalloc-0c49fdfe9affb3dd3
 #}
 #
+
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "terraform-up-and-running-state-nikita"
+    key            = "global/s3/terraform.tfstate"
+    region         = "eu-central-1"
+    # Replace this with your DynamoDB table name!
+    dynamodb_table = "terraform-up-and-running-locks-nikita"
+    encrypt        = true
+  }
+}
