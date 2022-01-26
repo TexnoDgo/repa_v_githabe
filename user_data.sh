920aa86d0480d21eb4191e629795aac2635f36c5
TexnoDgo <texnodgo@gmail.com>
2022-01-26 23:24:03 +0200

    Update user_data.sh

--- user_data.sh
+++ user_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Export variables
-#export last_version_ubuntu
+export last_version_ubuntu
 # Update system
 sudo apt update
 # Install docker
@@ -14,9 +14,9 @@ sudo apt install docker-ce -y
 sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 # Docker pull image
-docker pull texnodgo/apache2-image:${{last_version_ubuntu}}
+docker pull texnodgo/apache2-image:$last_version_ubuntu
 # Docker run
-sudo docker run -d -p 80:80 texnodgo/apache2-image:${{last_version_ubuntu}}
+sudo docker run -d -p 80:80 texnodgo/apache2-image:$last_version_ubuntu
 << EOF
 echo "$last_version_ubuntu"
 EOF
