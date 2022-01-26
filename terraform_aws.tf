8918ba68e5cd08913683d51504c3620451c622d6
TexnoDgo <texnodgo@gmail.com>
2022-01-26 20:48:06 +0200

    Update terraform_aws.tf

--- terraform_aws.tf
+++ terraform_aws.tf
@@ -27,28 +27,7 @@ resource "aws_instance" "Web_Ubuntu" {
   ami                    = data.aws_ami.latest_ubuntu.id
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.my_webserver_sg.id]
-  user_data              = <<EOF
-#!/bin/bash
-# Export variables
-export last_version_ubuntu
-# Update system
-sudo apt update
-# Install docker
-sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-sudo apt update
-apt-cache policy docker-ce
-sudo apt install docker-ce -y
-# Install docker_compose
-sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-# Docker pull image
-echo "last_version_ubuntu:$last_version_ubuntu"
-docker pull texnodgo/apache2-image:$last_version_ubuntu
-# Docker run
-sudo docker run -d -p 80:80 texnodgo/apache2-image:$last_version_ubuntu
-EOF
+  user_data              = file("user_data.sh")
 
 
   tags = {
