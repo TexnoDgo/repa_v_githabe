e77815498fba0423ad1f79267c3d26fc44b1ed4c
root <texnodgo@gmail.com>
2022-01-26 17:11:17 +0000

    change tf, sh

--- terraform_aws.tf
+++ terraform_aws.tf
@@ -1,30 +1,33 @@
+#Inizialisation AWS provider
 provider "aws" {
   region     = "eu-central-1"
 }
 
+# Search last version Ubuntu AMI
+data "aws_ami" "latest_ubuntu" {
+  owners      = ["099720109477"]
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+# Output last version  ubuntu ami
+output "test" {
+  value = data.aws_ami.latest_ubuntu
+}
+
+# Create Instance
 resource "aws_instance" "Web_Ubuntu" {
-  ami                    = "ami-0d527b8c289b4af7f"
+  ami                    = data.aws_ami.latest_ubuntu.id
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.my_webserver_sg.id]
-  user_data              = <<EOF
-#!/bin/bash
-# Update system
-sudo apt update
-# Install docker
-sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-sudo apt update
-apt-cache policy docker-ce
-sudo apt install docker-ce -y
-# Install docker_compose
-sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-# Docker pull image
-docker pull texnodgo/apache2-image
-# Docker run
-sudo docker run -d -p 80:80 texnodgo/apache2-image:latest
-EOF
+  user_data              = file("user_data.sh")
 
 
   tags = {
@@ -34,6 +37,7 @@ EOF
 
 }
 
+# Create Security Group
 resource "aws_security_group" "my_webserver_sg" {
   name        = "my_webserver_sg"
   description = "Allow TLS inbound traffic"
@@ -73,7 +77,7 @@ resource "aws_security_group" "my_webserver_sg" {
 #  allocation_id = eipalloc-0c49fdfe9affb3dd3
 #}
 
-
+# Save terraform backup to S3 backet
 terraform {
   backend "s3" {
     # Replace this with your bucket name!
